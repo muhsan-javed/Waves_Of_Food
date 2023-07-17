4bdf223ac593ec5610cc2574f168b30539a7cfa9
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-07-17 12:54:33 +0500

    Initial commit

---
+++ app/src/main/java/com/muhsanjaved/wavesoffood/Splash_Screen.kt
@@ -0,0 +1,21 @@
+package com.muhsanjaved.wavesoffood
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.muhsanjaved.wavesoffood.activities.StartActivity
+
+@Suppress("DEPRECATION")
+class Splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this,StartActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
+    }
+}
