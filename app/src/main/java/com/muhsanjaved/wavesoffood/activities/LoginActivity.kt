b70197aa41f63ac6adc129931713e5fe99704417
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-07-18 11:07:56 +0500

    Login Screen UI Completed

---
+++ app/src/main/java/com/muhsanjaved/wavesoffood/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.muhsanjaved.wavesoffood.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.muhsanjaved.wavesoffood.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/muhsanjaved/wavesoffood/activities/StartActivity.kt
+++ app/src/main/java/com/muhsanjaved/wavesoffood/activities/StartActivity.kt
@@ -1,12 +1,22 @@
 package com.muhsanjaved.wavesoffood.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.muhsanjaved.wavesoffood.R
+import com.muhsanjaved.wavesoffood.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private val binding : ActivityStartBinding by lazy {
+        ActivityStartBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        setContentView(binding.root)
+
+        binding.nextButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
