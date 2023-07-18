626cbecbd4382a0795d1f1e96c27a670f38fa7b6
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-07-18 11:43:03 +0500

    Sign Up Screen UI Completed

--- app/src/main/java/com/muhsanjaved/wavesoffood/activities/LoginActivity.kt
+++ app/src/main/java/com/muhsanjaved/wavesoffood/activities/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.muhsanjaved.wavesoffood.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.muhsanjaved.wavesoffood.R
+import com.muhsanjaved.wavesoffood.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val binding : ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/muhsanjaved/wavesoffood/activities/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.muhsanjaved.wavesoffood.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.muhsanjaved.wavesoffood.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
