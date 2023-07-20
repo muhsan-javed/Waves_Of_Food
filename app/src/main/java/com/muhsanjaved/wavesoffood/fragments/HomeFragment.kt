43a0db57685a37926f4d3964bbbc4a26ccee68df
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-07-20 11:50:07 +0500

    Home screen UI

--- app/src/main/java/com/muhsanjaved/wavesoffood/fragments/HomeFragment.kt
+++ app/src/main/java/com/muhsanjaved/wavesoffood/fragments/HomeFragment.kt
@@ -5,10 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
+import com.denzcoskun.imageslider.models.SlideModel
 import com.muhsanjaved.wavesoffood.R
+import com.muhsanjaved.wavesoffood.adapters.PopularAdapter
+import com.muhsanjaved.wavesoffood.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding : FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +30,41 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imageList = ArrayList<SlideModel>()
+        imageList.add(SlideModel(R.drawable.banner1, scaleType = ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner1, scaleType = ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner1, scaleType = ScaleTypes.FIT))
+
+        val imageSlide = binding.imageSlider
+        imageSlide.setImageList(imageList)
+        imageSlide.setImageList(imageList, ScaleTypes.FIT)
+
+        imageSlide.setItemClickListener(object :ItemClickListener {
+            override fun doubleClick(position: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(position: Int) {
+                val itemPosition = imageList[position]
+                val itemMessage = "Selected Image $position"
+                Toast.makeText(requireContext(), itemMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        val foodName = listOf("Burger","Sandwich","momo", "Herbal Pancake","Mixing")
+        val Price = listOf("$10","$8","$15", "$99","$50")
+        val popularFoodImages = listOf(R.drawable.menu1,R.drawable.menu2,R.drawable.menu3,R.drawable.menu4,R.drawable.menu6)
+        val adapter = PopularAdapter(foodName,Price,popularFoodImages)
+        binding.popularRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.popularRecyclerView.adapter = adapter
     }
 
     companion object {
