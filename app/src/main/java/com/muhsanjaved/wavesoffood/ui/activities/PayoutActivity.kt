b9e744c213d27c0dfbbba4b4f6e6a5cd553aed5c
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-07-26 11:05:38 +0500

    payout activity

---
+++ app/src/main/java/com/muhsanjaved/wavesoffood/ui/activities/PayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.muhsanjaved.wavesoffood.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.muhsanjaved.wavesoffood.R
+
+class PayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payout)
+    }
+}
