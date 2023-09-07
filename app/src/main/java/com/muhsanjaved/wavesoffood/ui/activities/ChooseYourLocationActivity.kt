80f022acd7f581c88cab9d626266f2a7133dd566
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-09-07 08:28:58 +0500

    remove code fix

--- app/src/main/java/com/muhsanjaved/wavesoffood/ui/activities/ChooseYourLocationActivity.kt
+++ app/src/main/java/com/muhsanjaved/wavesoffood/ui/activities/ChooseYourLocationActivity.kt
@@ -3,28 +3,31 @@ package com.muhsanjaved.wavesoffood.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.ArrayAdapter
 import com.muhsanjaved.wavesoffood.databinding.ActivityChooseYourLocationBinding
 import com.muhsanjaved.wavesoffood.ui.MainActivity
 
 class ChooseYourLocationActivity : AppCompatActivity() {
 
-    private val binding: ActivityChooseYourLocationBinding by lazy {
-        ActivityChooseYourLocationBinding.inflate(layoutInflater)
-    }
+    private lateinit var binding: ActivityChooseYourLocationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityChooseYourLocationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val locationList = arrayOf("Qambar", "Larkana", "Brohi Mohalla", "Ali Khan Kamber","karachi","Lahore")
+        val locationList =
+            arrayOf("Qambar", "Larkana", "Brohi Mohalla", "Ali Khan Kamber", "karachi", "Lahore")
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, locationList)
         val autoCompleteTextView = binding.listOfLocation
         autoCompleteTextView.setAdapter(adapter)
 
-        binding.desTextView.setOnClickListener {
-            val intent = Intent(this,MainActivity::class.java)
+        binding.btnGoToMainScreenButton.setOnClickListener {
+            val intent = Intent(this@ChooseYourLocationActivity, MainActivity::class.java)
             startActivity(intent)
+            finish()
         }
     }
 }
