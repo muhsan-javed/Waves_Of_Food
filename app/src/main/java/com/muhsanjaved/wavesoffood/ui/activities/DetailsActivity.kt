159a80110300bef577d400d457fb3325f6a88a47
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-11-01 13:47:10 +0500

    show data from Firebase database

--- app/src/main/java/com/muhsanjaved/wavesoffood/ui/activities/DetailsActivity.kt
+++ app/src/main/java/com/muhsanjaved/wavesoffood/ui/activities/DetailsActivity.kt
@@ -1,23 +1,38 @@
 package com.muhsanjaved.wavesoffood.ui.activities
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.muhsanjaved.wavesoffood.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailsBinding
+    private var foodName:String? =null
+    private var foodPrice:String? =null
+    private  var foodDescription:String? =null
+    private  var foodIngredient:String? =null
+    private var foodImage : String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val foodName = intent.getStringExtra("MenuItemName")
-        val foodImage = intent.getIntExtra("MenuItemImage", 1)
+        foodName = intent.getStringExtra("MenuItemName")
+        foodPrice = intent.getStringExtra("MenuItemPrice")
+        foodDescription = intent.getStringExtra("MenuItemDescription")
+        foodIngredient = intent.getStringExtra("MenuItemIngredient")
+        foodImage = intent.getStringExtra("MenuItemImage")
 
-        binding.detailFoodNameTextView.text = foodName
 
-        binding.detailImageView.setImageResource(foodImage)
+        with(binding){
+            detailFoodNameTextView.text = foodName
+            detailsShortDescriptionTextView.text = foodDescription
+            detailIngredientsTextView.text = foodIngredient
+            Glide.with(this@DetailsActivity).load(Uri.parse(foodImage)).into(detailImageView)
+        }
 
         binding.detailGoToBackImageButton.setOnClickListener {
             finish()
