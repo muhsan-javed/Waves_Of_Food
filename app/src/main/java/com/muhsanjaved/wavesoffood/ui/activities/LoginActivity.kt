16b54523adbb89eb5ef219d7bdc63385ed044e3b
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-11-01 11:11:33 +0500

    Login with Google Account

--- app/src/main/java/com/muhsanjaved/wavesoffood/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/muhsanjaved/wavesoffood/ui/activities/LoginActivity.kt
@@ -1,15 +1,20 @@
 package com.muhsanjaved.wavesoffood.ui.activities
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.database
@@ -41,8 +46,17 @@ class LoginActivity : AppCompatActivity() {
         database = Firebase.database.reference
         // Initialize Google Sign In
 
-        val googleSignInClient = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        // Configure Google Sign In
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_Id)).requestEmail().build()
 
+        googleSignInClient = GoogleSignIn.getClient(this,googleSignInOptions)
+
+        binding.googleLoginbutton.setOnClickListener {
+            val signIntent = googleSignInClient.signInIntent
+            launcher.launch(signIntent)
+        }
+        // END Google Sing In
 
         // Login Button
         binding.loginButton.setOnClickListener {
@@ -94,5 +108,30 @@ class LoginActivity : AppCompatActivity() {
         val currentUser = auth.currentUser
         if (currentUser != null)
             updateUI(currentUser)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            if (task.isSuccessful){
+                val account : GoogleSignInAccount = task.result
+                val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                auth.signInWithCredential(credential).addOnCompleteListener { authTask->
+                    // Successfully sign in with Google
+                    if (authTask.isSuccessful){
+                        Toast.makeText(this,"Successfully Sign-in with Google 😊", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                    else {
+                        Toast.makeText(this,"Sign-in Failed 😒", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }else {
+            Toast.makeText(this,"Sign-in Failed 😒", Toast.LENGTH_SHORT).show()
+        }
     }
 }
