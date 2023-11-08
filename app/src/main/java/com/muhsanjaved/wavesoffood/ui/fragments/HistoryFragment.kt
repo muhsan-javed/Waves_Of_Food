ee5c93a38979a336fd0a73855ae88a9ab76bfc6d
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-11-08 13:39:54 +0500

    setData RecentBuy

--- app/src/main/java/com/muhsanjaved/wavesoffood/ui/fragments/HistoryFragment.kt
+++ app/src/main/java/com/muhsanjaved/wavesoffood/ui/fragments/HistoryFragment.kt
@@ -1,18 +1,33 @@
 package com.muhsanjaved.wavesoffood.ui.fragments
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.muhsanjaved.wavesoffood.R
 import com.muhsanjaved.wavesoffood.adapters.BuyAgainAdapter
 import com.muhsanjaved.wavesoffood.databinding.FragmentHistoryBinding
+import com.muhsanjaved.wavesoffood.models.OrderDetails
 
 class HistoryFragment : Fragment() {
 
     private lateinit var binding: FragmentHistoryBinding
+    private lateinit var buyAgainAdapter: BuyAgainAdapter
+    private lateinit var database : FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
+    private lateinit var userId:String
+    private var listOfOrderItem:MutableList<OrderDetails> = mutableListOf()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +40,13 @@ class HistoryFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentHistoryBinding.inflate(inflater, container, false)
+
+        // Initialize Firebase
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+
+        // Retrieve and  display the user order History
+        retrieveBuyHistory()
 
         val historyFoodName =
             listOf(
@@ -75,19 +97,54 @@ class HistoryFragment : Fragment() {
             R.drawable.menu2
         )
 
-        val adapter =
-            BuyAgainAdapter(
-                ArrayList(historyFoodName),
-                ArrayList(historySellerName),
-                ArrayList(historyPrice),
-                ArrayList(historyImages)
-            )
+        val adapter = BuyAgainAdapter(ArrayList(historyFoodName),ArrayList(historySellerName),ArrayList(historyPrice),ArrayList(historyImages))
         binding.historyBuyAgainRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.historyBuyAgainRecyclerView.adapter = adapter
 
         return binding.root
     }
 
+    private fun retrieveBuyHistory() {
+        binding.recentBuyItem.visibility = View.INVISIBLE
+        userId = auth.currentUser?.uid?:""
+
+        val buyItemReference :DatabaseReference = database.reference.child("user").child(userId).child("BuyHistory")
+        val shortingQuery = buyItemReference.orderByChild("currentTime")
+
+        shortingQuery.addListenerForSingleValueEvent(object :ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (buySnapshot in snapshot.children){
+                    val buyHistoryItem = buySnapshot.getValue(OrderDetails::class.java)
+                    buyHistoryItem?.let {
+                        listOfOrderItem.add(it)
+                    }
+                }
+
+                listOfOrderItem.reverse()
+                if (listOfOrderItem.isNotEmpty()){
+                    setDataInRecentBuyItem()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    private fun setDataInRecentBuyItem() {
+        binding.recentBuyItem.visibility = View.VISIBLE
+        val recentOrderItem = listOfOrderItem.firstOrNull()
+        recentOrderItem?.let {
+            with(binding){
+                historyOrderedFoodName.text = it.foodNames?.firstOrNull()?:""
+                historyOrderedFoodPrice.text = it.foodPrices?.firstOrNull()?:""
+                val image = it.foodImage?.firstOrNull()?:""
+                val uri  = Uri.parse(image)
+                Glide.with(requireContext()).load(uri).into(historyOrderedImageView)
+            }
+        }
+    }
     private fun setupRecyclerView() {
 
     }
