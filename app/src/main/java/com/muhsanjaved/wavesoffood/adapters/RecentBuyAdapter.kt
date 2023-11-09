15e8a9c4b630ec7b14901f09d789bb8bb4643393
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-11-09 11:30:34 +0500

    Create Recent Cart Item  Adapter

---
+++ app/src/main/java/com/muhsanjaved/wavesoffood/adapters/RecentBuyAdapter.kt
@@ -0,0 +1,46 @@
+package com.muhsanjaved.wavesoffood.adapters
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.muhsanjaved.wavesoffood.databinding.RecentCartItemBinding
+
+class RecentBuyAdapter(
+    private var context :Context,
+    private var foodNameList :ArrayList<String>,
+    private var foodPriceList :ArrayList<String>,
+    private var foodImageList :ArrayList<String>,
+    private var foodQuantityList :ArrayList<String>,
+): RecyclerView.Adapter<RecentBuyAdapter.RecentViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecentBuyAdapter.RecentViewHolder {
+        val binding = RecentCartItemBinding.inflate(LayoutInflater.from(context),parent,false)
+        return RecentViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecentBuyAdapter.RecentViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int = foodNameList.size
+
+    inner class RecentViewHolder(val binding: RecentCartItemBinding):RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            binding.apply {
+                recentFoodName.text = foodNameList[position]
+                recentFoodPrice.text = foodPriceList[position]
+                recentQuantityCountTextView.text = foodQuantityList[position]
+                val uriString = foodImageList[position]
+                val uri = Uri.parse(uriString)
+                Glide.with(context).load(uri).into(recentFoodImage)
+            }
+        }
+
+    }
+}
