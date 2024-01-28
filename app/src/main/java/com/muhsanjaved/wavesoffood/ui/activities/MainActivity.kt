f847b91465f4330469d42a3918eb996f33ad40ff
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2024-01-28 16:06:33 +0500

    space

--- app/src/main/java/com/muhsanjaved/wavesoffood/ui/activities/MainActivity.kt
+++ app/src/main/java/com/muhsanjaved/wavesoffood/ui/activities/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         var NavController = findNavController(R.id.fragmentContainerView)
         var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNav.setupWithNavController(NavController)
